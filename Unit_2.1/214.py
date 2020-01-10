ab3298bbc3cb92c2c74647b694928dfb389f70c1
Eliana Johnson <johnsone11@student.psdr3.org>
2020-01-10 11:37:33 -0600

    fix filename

---
+++ Unit_2.1/214.py
@@ -0,0 +1,19 @@
+##############################################################################
+#   a113_TR_simple_window1.py
+#   Example soulution: Change its size to 200 by 100 pixels.
+##############################################################################
+import tkinter as tk
+
+# main window
+root = tk.Tk()
+root.wm_geometry("200x100")
+root.title("Authorization")
+
+# create empty frame
+frame_login = tk.Frame(root)
+frame_login.grid()
+
+lbl_username = tk.Label(frame_login, text='Username:')
+lbl_username.pack()
+
+root.mainloop()
